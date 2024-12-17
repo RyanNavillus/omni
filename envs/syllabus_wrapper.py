27fea40b81059aa525d414d98c8ca0998feb25da
Ryan Sullivan <ryanpnavillus@gmail.com>
2024-12-17 02:57:24 -0500

    Add syllabus file

---
+++ envs/syllabus_wrapper.py
@@ -0,0 +1,18 @@
+from syllabus.core import TaskWrapper
+from syllabus.task_space import DiscreteTaskSpace
+
+
+class CrafterTaskWrapper(TaskWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.env = env
+        self.task_space = DiscreteTaskSpace(len(self.env.target_achievements))
+
+    def reset(self, *args, **kwargs):
+        if "new_task" in kwargs:
+            new_task = kwargs.pop("new_task")
+            self.task = new_task
+        self.task_enc = self.env._encode_task(self.env.task_idx)
+
+        obs, info = self.env.reset()
+        return obs, info
