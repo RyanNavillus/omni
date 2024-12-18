5e0bbbb3690be63379b34c56ba0432c61204bb40
Ryan Sullivan <ryanpnavillus@gmail.com>
2024-12-18 00:21:00 -0500

    Change tasks

--- envs/syllabus_wrapper.py
+++ envs/syllabus_wrapper.py
@@ -12,6 +12,11 @@ class CrafterTaskWrapper(TaskWrapper):
         if "new_task" in kwargs:
             new_task = kwargs.pop("new_task")
             self.task = new_task
-        self.task_enc = self.env._encode_task(self.env.task_idx)
+            self.change_task(new_task)
         obs, info = self.env.reset()
         return obs, info
+
+    def change_task(self, new_task):
+        self.env.task_idx = new_task
+        self.env.task_steps = 0
+        self.env.task_enc = self.env._encode_task(self.task_idx)
