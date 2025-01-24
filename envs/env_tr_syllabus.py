4f2539e97e0f1e63db20c5a27621cb72abe0c3c9
Ryan Sullivan <ryanpnavillus@gmail.com>
2025-01-24 14:07:54 -0500

    Set dummy bits back to 10

--- envs/env_tr_syllabus.py
+++ envs/env_tr_syllabus.py
@@ -30,7 +30,7 @@ class Env(env.Env):
 
     def __init__(
             self, area=(64, 64), view=(9, 9), size=(64, 64),
-            reward=True, length=1500, seed=None, eval_mode=False, dummy_bits=1, **kwargs):
+            reward=True, length=1500, seed=None, eval_mode=False, dummy_bits=10, **kwargs):
         # TODO: Remove Hack
         global DUMMY_BITS, DUMMY_TASKS
         DUMMY_BITS = dummy_bits
